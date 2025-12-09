e9d2d973233497062e81f627e131fa337b2aa7fa
Eggmansmile <ruben.ryckaert89@gmail.com>
2025-12-09 15:28:43 +0100

    Fix: Set base path for project pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
-      base: '/',
+      base: '/Ruben-Ryckaert-s-CV/',
       server: {
         port: 3000,
         host: '0.0.0.0',
